c584cf8f92971d226e9c91213206c61be097c1a1
Alexander Shipin <alexander.shipin@gmail.com>
2017-05-15 19:12:49 +0300

    currency exchanger

---
+++ CurrencyExchanger/CurrencyRateResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CurrencyExchanger
+{
+	public class CurrencyRateResponse
+	{
+		public string @base { get; set; }
+		public DateTime date { get; set; }
+		public Dictionary<string, decimal> rates { get; set; }
+	}
+}
